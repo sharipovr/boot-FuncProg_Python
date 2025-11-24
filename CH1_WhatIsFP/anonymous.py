bc9b52824697a5587533a148e5889eab429eafbb
sharipovr <sharipov.rustem@gmail.com>
2025-11-23 21:34:36 -0600

    Completed CH2 - Map

--- CH1_WhatIsFP/anonymous.py
+++
@@ -1,7 +0,0 @@
-def file_type_getter(file_extension_tuples):
-    result = {}
-    for el in file_extension_tuples:
-        for ext in el[1]:
-            result[ext] = el[0]
-    
-    return lambda x: result.get(x, "Unknown")
